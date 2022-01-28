b3541db293f8186ba3c74a13ba74bbdfc97c681a
Auri <me@auri.xyz>
2022-01-27 16:17:19 -0800

    Finish refactoring common by removing obsolete Page, Graph, and Element types.

--- common/src/index.ts
+++ common/src/index.ts
@@ -1,14 +1,23 @@
-
-export * as Page from './Page';
-export * as Graph from './graph/index';
-export * as Definition from './definition';
-
 export { traverse, buildObjectPath } from './traverse';
 
-export { bytes as formatBytes, vector as formatVector,
-	date as formatDate, identifier as toIdentifier, titleCase } from './format';
+export {
+	bytes as formatBytes,
+	vector as formatVector,
+	date as formatDate,
+	identifier as toIdentifier,
+	titleCase,
+} from './format';
 
-export { Color, Hex, RGBA, HSVA, isHex, isRGBA, isHSVA, to } from './color';
+export {
+	Color,
+	Hex,
+	RGBA,
+	HSVA,
+	isHex,
+	isRGBA,
+	isHSVA,
+	to as convertColor,
+} from './color';
 
 export { merge, sign } from './util';
 
