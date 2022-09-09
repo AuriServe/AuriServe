e5e69d2fb4c7f0b4bfbeae34bc3da0b73f5dec61
Aurailus <me@auri.xyz>
2022-09-09 00:57:10 -0700

    Improved Calendar Styling

--- plugins/calendar/src/dashboard/CalendarPage.tsx
+++ plugins/calendar/src/dashboard/CalendarPage.tsx
@@ -1,6 +1,34 @@
 import { h } from 'preact';
-import { tw, AppContext, Button, Icon } from 'dashboard';
 import { useContext, useLayoutEffect } from 'preact/hooks';
+import { tw, AppContext, Button, Icon, Svg } from 'dashboard';
+
+const WEEKDAYS = [ 'Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat' ];
+
+const EVENTS = [{
+	ind: 3,
+	len: 2,
+	title: 'At Your Service',
+	theme: 'amber'
+}, {
+	ind: 8,
+	len: 1,
+	title: 'Tiller\'s Folly',
+	theme: 'amber'
+}, {
+	ind: 16,
+	len: 3,
+	title: 'The Wardens'
+}, {
+	ind: 16,
+	len: 2,
+	title: 'Early Morning Rain',
+	theme: 'amber'
+}, {
+	ind: 16,
+	len: 2,
+	title: 'Early Morning Rain II',
+	theme: 'amber'
+}];
 
 
 export default function CalendarPage() {
@@ -11,27 +39,33 @@ export default function CalendarPage() {
 		<div class={tw`flex -mb-14`}>
 			<div class={tw`shrink-0 w-72 bg-gray-800 border-(r gray-700) relative z-10`}/>
 			<div class={tw`grow flex-(& col)`}>
-				<div class={tw`shrink-0 bg-gray-800 flex justify-between items-center p-3 border-(b gray-700)`}>
-					<h2 class={tw`text-xl ml-2 leading-none font-medium`}>September 2022</h2>
+				<div class={tw`shrink-0 flex justify-between items-center px-2 py-3`}>
+					<div class={tw`flex items-center`}>
+						<Svg src={Icon.calendar} size={6}/>
+						<h2 class={tw`text-xl leading-none font-medium ml-2`}>September 2022</h2>
+					</div>
 					<div class={tw`flex gap-3`}>
-						<Button.Tertiary icon={Icon.view} label='Month'/>
-						<Button.Secondary icon={Icon.add} label='Add Event'/>
+						<Button.Secondary small icon={Icon.add} label='Add Event'/>
 					</div>
 				</div>
-				<div class={tw`shrink-0 h-10 bg-gray-800/50 grid-(& cols-7) gap-px relative z-10 shadow border-(b gray-700)`}>
-					{[ 'Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat' ].map((day) => <div key={day}
-						class={tw`text-center relative font-medium leading-none flex items-center justify-center`}>
-						<p class={tw`${day === 'Tue' && 'bg-accent-400 text-gray-900 inline-block px-3 py-1 rounded-full'}`}>
-							{day}
-						</p>
-					</div>)}
-				</div>
-				<div class={tw`grow bg-gray-700/75 grid-(& cols-7 rows-5) gap-px`}>
+				<div class={tw`grow bg-gray-900 grid-(& cols-7 rows-5) gap-1.5 p-2 pt-0`}>
 					{Array.from(Array(7 * 5).keys()).map((_, i) => <div key={i}
-						class={tw`${(i < 4 || i > 7 * 4 + 2) ? 'bg-gray-900/75' : 'bg-gray-900'}`}>
-						<p class={tw`mt-2 ml-2 w-5 h-5 mr-auto pt-px text-gray-200 text-center font-bold text-sm
+						class={tw`relative rounded-md p-2 pt-9 flex-(& col) gap-2 ${(i < 4 || i > 7 * 4 + 2) ? 'bg-gray-800/25' : 'bg-gray-800/50'}`}>
+						<p class={tw`absolute top-2 left-2 w-5 h-5 mr-auto pt-px text-gray-200 text-center font-bold text-sm
 							${i === 9 && `rounded-full bg-accent-400 text-gray-900
-							ring-(2 accent-500/25 offset-2 offset-gray-900)`}`}>{i}</p>
+							ring-(& accent-500/25 offset-gray-900)`}`}>{i}</p>
+						{i < 7 && <p class={tw`absolute top-2.5 left-10 pt-px text-gray-500 font-bold text-xs uppercase tracking-widest`}>
+							{WEEKDAYS[i]}
+						</p>}
+						{EVENTS.filter(event => event.ind === i).map(event => <div key={event.title} class={tw`
+							bg-gray-750 rounded theme-${event.theme ?? 'blue'} relative
+							p-1.5 px-2.5 text-sm font-bold flex gap-2.5 items-center relative z-10 shadow-md text-accent-100
+							hover:(bg-gray-700 text-accent-50) transition duration-75 cursor-pointer`}
+							style={{ width: `calc(${event.len} * (100% + 14px) + ${event.len - 1} * (8px) - 14px`}}>
+							<div class={tw`w-2 h-2 shrink-0 ring-(& accent-400/25) bg-accent-400 rounded-full`}/>
+							<p>{event.title}</p>
+							<div class={tw`absolute w-16 h-full left-0 rounded-l bg-gradient-to-r from-accent-400/20 via-accent-400/5 to-transparent interact-none`}/>
+						</div>)}
 					</div>)}
 				</div>
 			</div>
